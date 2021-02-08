06449f936fa7818381c13b3105c3cfa4f39a08c7
Quirky Girl Coder <nadiamormon@Nadias-Air.attlocal.net>
2021-02-08 07:25:47 -0500

    change checkout method

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ const calculateOrderAmount = items => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
-  return 1400;
+  return 45000;
 };
 app.post("/create-payment-intent", async (req, res) => {
   const { items } = req.body;
